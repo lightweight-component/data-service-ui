454fc23f4e062aa8f488ce3c04db4e9d2e61451e
sp42@qq.com <sp42@qq.com>
2024-07-10 09:21:18 +0800

    ci

--- src/index.vue
+++ src/index.vue
@@ -4,83 +4,17 @@
 
 <script>
 import DataService from "./data-service/data-service.vue";
-// import DataServiceIndex from "../components/data-service/index/index.vue";
-import { setBaseQueryString } from '@ajaxjs/util/dist/util/xhr';
+import aj from '@ajaxjs/ui/dist/';
 
-function getQueryParam(variable, isParent) {
-  var query = (isParent ? parent.location : window.location).search.substring(
-    1
-  );
-  var vars = query.split("&");
+const loginUrl = 'http://127.0.0.1:8088/base/user/login';
+const thisPageUrl = 'http://localhost:8080';
 
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-
-    if (pair[0] == variable) return pair[1];
-  }
-
-  return false;
-}
-
-window.loginUrl = "http://127.0.0.1:8088/base/user/login?web_url=http://localhost:8081";
-const token = getQueryParam("token");
-let accessToken = localStorage.getItem("accessToken");
-
-if (!accessToken && !token) {
-  // alert("你未登录！");
-  // location.assign(loginUrl);
-}
-
-if (token) {
-  accessToken = decodeURIComponent(token);
-  localStorage.setItem("accessToken", accessToken);
-
-  // 只需要第一次的参数，之后不需要，现在清除
-  const url = new URL(location.href); // 创建一个包含查询参数的URL
-  const params = new URLSearchParams(url.search); // 获取URL中的查询参数
-  params.delete("token"); // 删除名为'b'的参数
-  url.search = params.toString(); // 更新URL的查询参数
-
-  location.assign(url.href);
-}
-
-// window.JWT_TOKEN = JSON.parse(accessToken);
-
-// 将JWT Token拆分为三个部分
-// const tokenParts = window.JWT_TOKEN.id_token.split(".");
-// const tokenParts = accessToken.split(".");
-// const payload = JSON.parse(atob(tokenParts[1])); // 解析载荷
-// console.log(payload.aud);
-
-// // 租户 id
-// const arr = payload.aud.match(/(?<=tenantId=)\d+/);
-
-// if (arr) {
-//     const tenantId = arr[0];
-//     setBaseQueryString({ tenantId: tenantId });
-// }
-
-// 监听来自A域的网页发送的消息
-window.addEventListener("message", function (event) {
-  // if (event.origin === 'http://A域的网页地址') {
-  console.log("收到来自A域的网页的消息：", event.data);
-
-  if (event.data === "doLogout") {
-    localStorage.removeItem("accessToken");
-  }
-  // }
-});
+aj.IAM.getLoginInfo(loginUrl, thisPageUrl);
 
 export default {
   components: {
     DataService
   },
-  data() {
-    return {
-
-    };
-  },
-
   methods: {
     routeTo(route) {
       location.hash = "#/" + route;
